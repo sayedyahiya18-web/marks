00be63a5b743f332ab19a58ca69f768c101798a1
sayedyahiya18-web <sayedyahiya18@gmail.com>
2025-11-21 10:04:18 +0530

    save this file

---
+++ marks.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv)==4:
+    m1 = float(sys.argv[1])
+    m2 = float(sys.argv[2])
+    m3 = float(sys.argv[3])
+
+else:
+    m1 = m2 = m3 =80
+
+total = m1+m2+m3
+if total<40:
+    print("Status : fail")
+else:
+    print("Status  :Pass")
